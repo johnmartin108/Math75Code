c06b1c59062c89adefa9cdb8e3167b07282e5199
John <Jmr7in108@gmail.com>
2016-05-24 20:02:25 -0400

    explained q1 code

--- HW8/q1_code.py
+++ HW8/q1_code.py
@@ -1,5 +1,5 @@
 ZZnZZ = Integers(27781703927)
 
-AtoB = ZZnZZ(5)**1002883876
-BtoA = ZZnZZ(5)**21790753397
-SK = ZZnZZ(AtoB)**21790753397
+AtoB = ZZnZZ(5)**1002883876 #Alice chooses a, sends Bob g^a
+BtoA = ZZnZZ(5)**21790753397 #Bob chooses b, sends Alice g^b
+SK = ZZnZZ(AtoB)**21790753397 #secret key is g^ab, or (g^a)^b
